6a92d61d0ccd271292ed5df0c7cb9202aa8ce23e
lilweirdward <zachw38@gmail.com>
2023-06-15 13:24:35 -0400

    Fixed bug with Discord User ID not saving

--- src/Repository/DTO/BaseAccountRequest.cs
+++ src/Repository/DTO/BaseAccountRequest.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace Repository.DTO;
 
 public abstract class BaseAccountRequest
@@ -5,6 +7,8 @@ public abstract class BaseAccountRequest
     public char Role { get; set; }
     public string UniqueCode { get; set; }
     public string Discord { get; set; }
+    
+    [JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
     public long DiscordUniqueId { get; set; }
     public bool HasForeignMinistry { get; set; }
     public bool HasFederalAidCommission { get; set; }
